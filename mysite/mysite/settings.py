b18aeb0c32a0c0ee97c5ae9b08e210e451a2c019
Yuri Ulianov <68346643+yukeeul@users.noreply.github.com>
2022-12-20 14:40:29 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bt)sa+(w6j039poc$7kz5nht7t69f0*#lhmj*%+svuothx11em'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.54.112']
 
