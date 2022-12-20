916de2ebb4f8482b2efc4505e124c7e8401f3b4d
Yuri Ulyanov <yuli1108@mail.ru>
2022-12-21 02:40:45 +0300

    .

--- mysite/flights/views.py
+++ mysite/flights/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.apps import apps
 
 def flights_view(request):
+    print(request)
     try:
         city_from = request.GET['var1']
         city_to = request.GET['var2']
@@ -14,4 +15,12 @@ def flights_view(request):
     ans = []
     for flight in model.objects.raw('SELECT * FROM flights_flight WHERE city_from="{}" AND city_to="{}" AND Date(departure_date)="{}"'.format(city_from, city_to, date)):
         ans.append({"city_from" : flight.city_from, "city_to" : flight.city_to, "price" : flight.price, "departure_date" : flight.departure_date, "arrival_date" : flight.arrival_date})
-    return JsonResponse({'ans': ans})
+        
+    response = JsonResponse({'ans': ans})
+    response["Access-Control-Allow-Origin"] = "*"
+    response['Content-Type'] = "application/json; charset=utf-8"
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type, My-Token"
+    return response
