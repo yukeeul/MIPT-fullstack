59363efedbda7336e6b9afc1c0f89e431e034ea2
Yuri Ulianov <68346643+yukeeul@users.noreply.github.com>
2022-12-20 14:39:27 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bt)sa+(w6j039poc$7kz5nht7t69f0*#lhmj*%+svuothx11em
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.54.112']
 
 
 # Application definition
