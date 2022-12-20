319c291e22e20334f61781a5ac08e3384b2d3d02
Yuri Ulyanov <yuli1108@mail.ru>
2022-12-20 14:55:01 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function isEmail() {
     let var2 = document.getElementById("uniqueID2").value;
     let var3 = document.getElementById("uniqueID3").value;
     let request = new XMLHttpRequest();
-    url = "http://127.0.0.1:8000?var1=" + var1 + "&var2=" + var2 + "&var3=" + var3;
+    url = "http://158.160.54.112?var1=" + var1 + "&var2=" + var2 + "&var3=" + var3;
     request.open("GET", url);
     request.onreadystatechange = function () {
        if (request.readyState === 4) {
