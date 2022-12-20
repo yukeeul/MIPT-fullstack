2523065ae00b01c0fb97b6fe547f9889d5a617a5
Yuri Ulyanov <yuli1108@mail.ru>
2022-12-21 01:21:22 +0300

    .

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,16 +1,17 @@
-# importing HttResponse from library
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.apps import apps
 
 def home(request):
-    # request is handled using HttpResponse object
-    print("I am working")
-    print(str(request))
-    print(request.headers)
     try:
-        print(request.GET['var1'])
-        print(request.GET['var2'])
-        print(request.GET['var3'])
+        city_from = request.GET['var1']
+        city_to = request.GET['var2']
+        date = request.GET['var3']
     except:
-        pass
-    return HttpResponse("random text")
-    #return HttpResponse("Any kind of HTML Here")
+        return HttpResponse("incorrect request")
+    
+    model = apps.get_model('flights', 'Flight')
+    
+    ans = []
+    for flight in model.objects.raw('SELECT * FROM flights_flight WHERE city_from="{}" AND city_to="{}" AND Date(departure_date)="{}"'.format(city_from, city_to, date)):
+        ans.append({"city_from" : flight.city_from, "city_to" : flight.city_to, "price" : flight.price, "departure_date" : flight.departure_date, "arrival_date" : flight.arrival_date})
+    return JsonResponse({'ans': ans})
