03538f3a960a3cbb2b636a6d810fcfd7b5358921
Yuri Ulyanov <yuli1108@mail.ru>
2022-12-21 11:13:16 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
+import Login from './Login';
+
 
 import { useState} from 'react';
 
+function setToken(userToken) {
+  alert('setToken');
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  return tokenString;
+}
+
 
 function App() {
+  const token = getToken();
+  //const token = getToken();
   const [answer, setAnswer] = useState([]);
   const [var1, setVar1] = useState('');
   const [var2, setVar2] = useState('');
   const [var3, setVar3] = useState('');
   
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   const getApiData = async () => {
       const response = await fetch(
         "http://158.160.54.112:8000?var1=" + var1 + "&var2=" + var2 + "&var3=" + var3
@@ -25,8 +43,6 @@ function App() {
       console.log(typeof arr);
   };
    
-   
-
   const handleChange1 = (event) => {
     setVar1(event.target.value);
   };
@@ -37,9 +53,14 @@ function App() {
     setVar3(event.target.value);
   };
   
-  const handleClick = () => {
+  const handleSearchClick = () => {
     getApiData();
   };
+  
+  const handleBuyClick = (e) => {
+    let id = e.target.id;
+    alert(e.target.id);
+  }
 
 
   return (
@@ -49,19 +70,19 @@ function App() {
           <input type="text" id="uniqueID2" onChange={handleChange1} value={var1} placeholder="Откуда"/>
           <input type="text" id="uniqueID2" onChange={handleChange2} value={var2} placeholder="Куда"/>
           <input type="text" id="uniqueID3" onChange={handleChange3} value={var3} placeholder="ГГГГ-ММ-ДД"/>
-          <button id="find-button" onClick={handleClick}> Нажми на меня</button>
+          <button id="find-button" onClick={handleSearchClick}> Нажми на меня</button>
       </div>
     </div >
     
     <div id="main-div">  
-        {answer.map(flight => (
-          <div id="flight-container">
-            <div class="text-div"> <p> {flight[0]} </p> </div>
-            <div class="text-div"> <p> {flight[1]} </p> </div>
-            <div class="text-div"> <p> {flight[2]} </p> </div>
-            <div class="text-div"> <p> {flight[3]} </p> </div>
-            <div class="text-div"> <p> {flight[4]} </p> </div>
-            <div class="text-div"> <p> temp text </p> </div>
+        {answer.map((flight, index) => (
+          <div key={index} className="flight-container">
+            <div className="text-div"> <p> {flight[0]} </p> </div>
+            <div className="text-div"> <p> {flight[1]} </p> </div>
+            <div className="text-div"> <p> {flight[2]} </p> </div>
+            <div className="text-div"> <p> {flight[3]} </p> </div>
+            <div className="text-div"> <p> {flight[4]} </p> </div>
+            <div className="text-div"> <button id={index} onClick={handleBuyClick}> temp text </button> </div>
           </div>
         ))}
 
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,17 +3,19 @@ from django.apps import apps
 
 def home(request):
     print(request)
-    try:
-        city_from = request.GET['var1']
-        city_to = request.GET['var2']
-        date = request.GET['var3']
-    except:
-        return HttpResponse("incorrect request")
-    
-    model = apps.get_model('flights', 'Flight')
-    
-    ans = []
-    for flight in model.objects.raw('SELECT * FROM flights_flight WHERE city_from="{}" AND city_to="{}" AND Date(departure_date)="{}"'.format(city_from, city_to, date)):
-        ans.append({"city_from" : flight.city_from, "city_to" : flight.city_to, "price" : flight.price, "departure_date" : flight.departure_date, "arrival_date" : flight.arrival_date})
+    if request.method == 'GET':
+        try:
+            city_from = request.GET['var1']
+            city_to = request.GET['var2']
+            date = request.GET['var3']
+        except:
+            return HttpResponse("incorrect request")
         
-    return JsonResponse({'ans': ans})
+        model = apps.get_model('flights', 'Flight')
+        
+        ans = []
+        for flight in model.objects.raw('SELECT * FROM flights_flight WHERE city_from="{}" AND city_to="{}" AND Date(departure_date)="{}"'.format(city_from, city_to, date)):
+            ans.append({"city_from" : flight.city_from, "city_to" : flight.city_to, "price" : flight.price, "departure_date" : flight.departure_date, "arrival_date" : flight.arrival_date})
+            
+        return JsonResponse({'ans': ans})
+        
