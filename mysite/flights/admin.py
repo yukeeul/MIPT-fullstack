6c4353e945b2bca41c2c9d9d1e41debfa81b78bc
Yuri Ulyanov <yuli1108@mail.ru>
2022-12-21 11:19:34 +0300

    .

--- mysite/flights/admin.py
+++ mysite/flights/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Flight, Ticket
+from .models import Flight
 # Register your models here.
 
 admin.site.register(Flight)
-admin.site.register(Ticket)
